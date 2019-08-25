7733f1cf2562635b01759d9c8e669c1e8e687433
goran995 <goran.ggooxxyy@gmail.com>
2019-08-25 18:53:48 +0200

    first commit

---
+++ Zadaci/Program.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaci
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Zadatak 1
+            //Console.WriteLine("Goran");
+            //Console.Write("Milosevic");
+
+            //Zadatak 2
+            //Console.Write("Goran\nMilosevic");
+
+            //Zadatak 3
+            //Console.Write("Goran Milosevic\t2i1007018");
+
+            //Zadatak 3 a
+            //Console.WriteLine("Goran Filip Konj");
+            //Console.Write("Konj Filip Goran");
+
+            //Zadatak 3 a i b
+            //Console.WriteLine("Goran Filip Konj");
+            //Console.WriteLine("Goran konj Filip");
+
+            //Zadatak 3 a i c
+            //Console.Write("Goran Konj Filip\nKonj Goran Filip");
+
+            //Zadatak 4 i 5 zajedno na dva nacina
+            //string ime_prezime_pol;
+            //Console.WriteLine("Unesite ime prezime i pol:");
+            //ime_prezime_pol = Console.ReadLine();
+            //int godine;
+            //Console.WriteLine("Unesite godine:");
+            //godine = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Vi ste: {0}", ime_prezime_pol);
+            //Console.Write("Imate {0} godina", godine);
+
+
+            //string ime_prezime;
+            //Console.WriteLine("unesite Ime i Prezime");
+            //ime_prezime = Console.ReadLine();
+            //string pol;
+            //Console.WriteLine("Unesite Pol");
+            //pol = Console.ReadLine();
+            //int godine;
+            //Console.WriteLine("Unesite Godine:");
+            //godine = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Vi ste: {0} ,", ime_prezime);
+            //Console.Write("Pol: {0} , Imate: {1} Godina", pol, godine);
+
+
+
+            //Zadatak 5
+            //string ime = "Goran";
+            //string prezime = "Milosevic";
+            //Console.Write("{0} {1}",ime , prezime);
+
+
+            //Zadatak 6
+            //int broj = 10;
+            //Console.Write("Broj je {0} :", broj);
+
+            //Zadatak 7
+            //int broj;
+            //Console.Write("Unesite broj:");
+            //broj = int.Parse(Console.ReadLine());
+            //int broj1;
+            //Console.Write("Unesite drugi broj:");
+            //broj1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Rezultat oduzimanja je: {0}", broj - broj1);
+
+            //Zadatak 8
+            //string broj = "2";
+            //string broj1 = "2";
+            //decimal a;
+            //decimal b;
+            //a = decimal.Parse(broj);
+            //b = decimal.Parse(broj1);
+            //Console.Write("Unesite decimalni broj: ");
+            //a = decimal.Parse(Console.ReadLine());
+            //Console.Write("Unesite drugi decimalni broj: ");
+            //b = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Rezultat deljenja je: {0}", a / b);
+
+
+            ////Zadatak 9
+            //int broj;
+            //int broj1;
+            //int broj2;
+            //Console.WriteLine("Unesite prvi broj: ");
+            //broj = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Unesite drugi broj: ");
+            //broj1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Unesite treci broj: ");
+            //broj2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Rezultat operacije a+b*c: {0}", broj + broj1 * broj2);
+
+
+            //Zadatak 10
+            //int broj;
+            //Console.WriteLine("Unesite prvi broj");
+            //broj = int.Parse(Console.ReadLine());
+            //int broj1;
+            //Console.WriteLine("Unesite drugi broj");
+            //broj1 = int.Parse(Console.ReadLine());
+            //int broj2;
+            //Console.WriteLine("Unesite treci broj");
+            //broj2 = int.Parse(Console.ReadLine());
+            //int broj3;
+            //Console.WriteLine("Unesite cetvrti broj");
+            //broj3 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Rezultat a+b+c+d je: {0}", broj + broj1 + broj2 + broj3 );
+            //Console.WriteLine("Rezultat a-b-c-d je: {0}", broj - broj1 - broj2 - broj3 );
+            //Console.WriteLine("Rezultat a/b/c/d je: {0}", broj / broj1 / broj2 / broj3 );
+            //Console.WriteLine("Rezultat a*b*c*d je: {0}", broj * broj1 * broj2 * broj3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
